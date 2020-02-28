36da7cf13882009c4f5254c39918705f9a3d2e80
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-28 15:22:30 +0200

    add post method for adding new article

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
-const Article = require('../models/Article')
+const Article = require('../models/Article');
 router.get('/', async (req, res) => {
-	const articles = await  Article.find({},{comments:0});
+	const articles = await Article.find({}, { comments: 0 ,__v:0});
 	res.json(articles);
 });
+// router for adding new article
+router.post('/', async (req, res) => {
+	try {
+		const article = new Article(req.body) ;
+		await  article.save() ;
+		res.json(article)
+	}
+	catch (e) {
+		res.send(e);
+	}
+});
 
-
-module.exports = router ;
+module.exports = router;

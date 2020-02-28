1db6dec9dc51696678ce97f28e453241079ee2f9
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-28 22:53:57 +0200

    add router for updating article with its itd

--- src/routes/article.js
+++ src/routes/article.js
@@ -16,35 +16,48 @@ router.post('/', async (req, res) => {
 });
 router.get('/:id', async (req, res) => {
 	try {
-		const article = await Article.findOne({ _id:req.params.id});
+		const article = await Article.findOne({ _id: req.params.id });
 		console.log(article.toString());
 		if (!article.id)
 			res.json({ error: true, message: "error can't find the article" });
-		res.json(article)
-	}
-	catch (e) {
+		res.json(article);
+	} catch (e) {
 		console.log(e);
 		res.json({ error: true, message: 'please try to pass the correct id' });
 	}
 });
-router.delete('/',async (req,res)=>{
-	const {articleId} = req.query ;
-	if(!articleId) {
-		res.json(
-			{error:true,message:"please provide articleId"}
-		);
+router.delete('/', async (req, res) => {
+	const { articleId } = req.query;
+	if (!articleId) {
+		res.json({ error: true, message: 'please provide articleId' });
 	}
 	try {
-		console.log(`articleId is ${articleId}`)
-		const article = await Article.deleteOne({_id:articleId}) ;
+		console.log(`articleId is ${articleId}`);
+		const article = await Article.deleteOne({ _id: articleId });
 		console.log(article);
-		res.json({done:true});
-	}
-	catch (e) {
+		res.json({ done: true });
+	} catch (e) {
 		res.json({
-			error:true,
-			message:e.message
-		})
+			error: true,
+			message: e.message,
+		});
 	}
-})
+});
+/*
+	router fo updating specific article
+	using its it and put method
+ */
+router.put('/:id', async (req, res) => {
+	console.log(req.params.id)
+	const { title, body } = req.body;
+	try {
+		const article = await Article.findOne({ _id: req.params.id });
+		article.title = title;
+		article.body = body;
+		await article.save();
+		res.json(article);
+	} catch (e) {
+		res.json({ error: true, message: e.message });
+	}
+});
 module.exports = router;

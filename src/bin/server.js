c1b48a606fea39b809ddf57a9dfd1b28187fff6a
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-28 15:12:16 +0200

    connect to mongodb using mongoose

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,7 +1,20 @@
 const app = require('../app');
 const http = require('http');
+const mongoose = require('mongoose');
 
+const DB_LOCAL_URL = 'mongodb://localhost:27017/blog';
 // app server
+mongoose
+	.connect(process.env.DATABASE_URL || DB_LOCAL_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('connected to db');
+	})
+	.catch(err => {
+		console.error(err);
+	});
 const server = http.createServer(app);
 
 // server port
